e034581d9a97050826a324cc5108b6f3b9a99ea6
mohammedAlmaghlout <mo.maghlouth@gmail.com>
2024-01-28 22:24:33 +0300

    Setting Style packages 🎨

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([BrowserModule, BrowserAnimationsModule]),
+  ],
 };
