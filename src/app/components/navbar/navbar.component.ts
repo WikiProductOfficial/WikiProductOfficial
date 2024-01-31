d4c9c04b2436465c82c9c0bf995b4b2e8acf1cd1
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2024-01-31 21:40:26 +0300

    navbar style for scroll

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [SidebarModule, ButtonModule, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  sidebarVisible: boolean = false;
+  navbarScrolled: boolean = false;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollOffset =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    this.navbarScrolled = scrollOffset > 0;
+  }
+
+  checkScreenSize() {
+    if (this.sidebarVisible) {
+      this.sidebarVisible = window.innerWidth < 768;
+    }
+  }
+  scrollTo(element: HTMLElement): void {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
 }
