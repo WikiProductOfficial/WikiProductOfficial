b7ea623cae5dfab5b9b15f8668f44627fb0aceff
Ammar <ammaralmajed7@gmail.com>
2024-02-01 19:44:23 +0300

    adding the templete

--- app/app/settings.py
+++ app/app/settings.py
@@ -33,6 +33,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',
+    'rest_framework',
+    'drf_yasg',
+    'corsheaders',
     'core',
 ]
 
