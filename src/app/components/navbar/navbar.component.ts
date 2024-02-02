e13800d5381ac8c181126e0a5d4a2cf177da7c58
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2024-02-02 18:13:59 +0300

    Creating categories sidebar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,30 +1,51 @@
-import { Component, HostListener, effect, signal } from '@angular/core';
+import { Component, HostListener, OnInit, effect, signal } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from '../../services/theme.service';
 import { CookieService } from 'ngx-cookie-service';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { TooltipModule } from 'primeng/tooltip';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, CommonModule],
+  imports: [
+    SidebarModule,
+    ButtonModule,
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    TooltipModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   sidebarVisible: boolean = false;
   navbarScrolled: boolean = false;
+  isLargeScreen: boolean = false;
+  categories!: string[];
   isDarkTheme = signal(this.cookieService.get('theme') === 'true');
+  value: any;
 
   constructor(
     private themeService: ThemeService,
-    private cookieService: CookieService
+    private cookieService: CookieService,
+    private categoriesService: CategoriesService
   ) {
     effect(() => {
       this.cookieService.set('theme', JSON.stringify(this.isDarkTheme()));
       this.themeService.toggleToDark(this.isDarkTheme());
     });
+  }
+  ngOnInit(): void {
+    this.categoriesService.getCategories().subscribe((data) => {
+      this.categories = data.categories;
+    });
+    this.checkScreenSize();
   }
 
   @HostListener('window:resize', ['$event'])
@@ -42,9 +63,7 @@ export class NavbarComponent {
   }
 
   checkScreenSize() {
-    if (this.sidebarVisible) {
-      this.sidebarVisible = window.innerWidth < 768;
-    }
+    this.isLargeScreen = window.innerWidth >= 1024;
   }
   scrollTo(element: HTMLElement): void {
     element.scrollIntoView({ behavior: 'smooth' });
