d176c21049ba1884767d1fb8209d03a2b84b70ac
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2024-02-03 18:50:53 +0300

    icon navigate to home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: HomePageComponent }];
