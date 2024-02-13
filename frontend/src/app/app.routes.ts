44ad061b7c8f29921b84d109fe28b80086869b52
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2024-02-13 17:10:55 +0300

    adding results page component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { ResultsPageComponent } from './components/results-page/results-page.component';
 
-export const routes: Routes = [{ path: '', component: HomePageComponent }];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'results', component: ResultsPageComponent },
+];
