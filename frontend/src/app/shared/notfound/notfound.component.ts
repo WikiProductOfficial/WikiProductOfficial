2b673cbf7c39847a1c86e512566065bd99dd671d
Mohammed alzowaid <102298394+Medo-02@users.noreply.github.com>
2024-02-15 19:44:20 +0300

    add buttons + (error code and error message) as input

--- frontend/src/app/shared/notfound/notfound.component.ts
+++ frontend/src/app/shared/notfound/notfound.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Location } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { Footer } from 'primeng/api';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { Router } from '@angular/router';
+import { Button, ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-notfound',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, ButtonModule],
   templateUrl: './notfound.component.html',
   styleUrl: './notfound.component.scss'
 })
 export class NotfoundComponent {
-  errorMessage = 'Oops! it seems that you are trying to access a page that does not exist';
+  constructor(private router: Router, private location:Location){}
+  @Input() errorCode: string = '404';
+  @Input() errorMessage: string = 'Oops! it seems that you are trying to access a page that does not exist';
+
+  goToHome(){
+    this.router.navigate(['/']);
+  }
+  goBack(){
+    this.location.back();
+  }
 }
