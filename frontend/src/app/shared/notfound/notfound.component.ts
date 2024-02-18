c730df70575589d8247a948d27d1a8482fc70c81
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2024-02-18 23:19:37 +0300

    removing app-component from notfound

--- frontend/src/app/shared/notfound/notfound.component.ts
+++ frontend/src/app/shared/notfound/notfound.component.ts
@@ -1,26 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { Location } from '@angular/common';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { FooterComponent } from '../../components/footer/footer.component';
 import { Router } from '@angular/router';
 import { Button, ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-notfound',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, ButtonModule],
+  imports: [ButtonModule],
   templateUrl: './notfound.component.html',
-  styleUrl: './notfound.component.scss'
+  styleUrl: './notfound.component.scss',
 })
 export class NotfoundComponent {
-  constructor(private router: Router, private location:Location){}
+  constructor(private router: Router, private location: Location) {}
   @Input() errorCode: string = '404';
-  @Input() errorMessage: string = 'Oops! it seems that you are trying to access a page that does not exist';
+  @Input() errorMessage: string =
+    'Oops! it seems that you are trying to access a page that does not exist';
 
-  goToHome(){
+  goToHome() {
     this.router.navigate(['/']);
   }
-  goBack(){
+  goBack() {
     this.location.back();
   }
 }
