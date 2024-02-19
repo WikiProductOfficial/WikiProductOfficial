0615ee354b5e1db423c593f3ba5a86fb7c6c4bec
Ammar <ammaralmajed7@gmail.com>
2024-02-19 06:39:20 +0300

    Adding the search function to be implemented

--- backend/app/core/views.py
+++ backend/app/core/views.py
@@ -14,6 +14,7 @@ from . import models, serializers
 
 @api_view(['POST'])
 def search(request):
+    ### Inital build  of the search function to test it - feel free to changed
     data = JSONParser().parse(request)
     query = data.get('query', '')
     if query:
