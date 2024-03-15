93418b7a69e16b7f16ca1cb4e21eeb2139566222
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2024-03-15 14:00:11 +0300

    add loader to search

--- nginx/frontend/src/app/components/results-page/products-data-view/products-data-view.component.ts
+++ nginx/frontend/src/app/components/results-page/products-data-view/products-data-view.component.ts
@@ -4,21 +4,25 @@ import { DataViewModule } from 'primeng/dataview';
 import { ButtonModule } from 'primeng/button';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
+import { SearchService } from '../../../services/search.service';
+import { LoaderComponent } from '../../../shared/loader/loader.component';
 
 @Component({
   selector: 'app-products-data-view',
   standalone: true,
+  templateUrl: './products-data-view.component.html',
+  styleUrl: './products-data-view.component.scss',
   imports: [
     DataViewModule,
     CommonModule,
     ButtonModule,
     RatingModule,
     FormsModule,
+    LoaderComponent,
   ],
-  templateUrl: './products-data-view.component.html',
-  styleUrl: './products-data-view.component.scss',
 })
 export class ProductsDataViewComponent {
+  constructor(public searchService: SearchService) {}
   @Input() products!: any[];
   layout: 'list' | 'grid' = 'grid';
 }
--- nginx/frontend/src/app/services/search.service.ts
+++ nginx/frontend/src/app/services/search.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, finalize, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -8,8 +8,10 @@ import { environment } from '../../environments/environment';
 })
 export class SearchService {
   constructor(private http: HttpClient) {}
+  loading: boolean = false;
 
   getProducts(query: string, page: string): Observable<any> {
+    this.loading = true;
     return this.http
       .get<any>(`${environment.backendUrl}/search/?query=${query}&page=${page}`)
       .pipe(
@@ -19,6 +21,9 @@ export class SearchService {
             () =>
               new Error('Failed to load JSON data; see console for details.')
           );
+        }),
+        finalize(() => {
+          this.loading = false; // loading is finished, remove spinner
         })
       );
   }
