85b55c923b2c5bfe1c3b1051f9030bc32391873e
Mohammed alzowaid <102298394+Medo-02@users.noreply.github.com>
2024-04-13 03:11:42 +0300

    Refactor the chat dialog
    -Add button to open the dialog
    -make the dialog responsive
    -make the dialog draggaable

--- nginx/frontend/src/app/components/chat-dialog/chat-dialog.component.ts
+++ nginx/frontend/src/app/components/chat-dialog/chat-dialog.component.ts
@@ -9,6 +9,7 @@ import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { ButtonModule } from 'primeng/button';
 import { map, scan } from 'rxjs/operators';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-chat-dialog',
@@ -20,11 +21,13 @@ import { map, scan } from 'rxjs/operators';
     InputGroupModule,
     InputGroupAddonModule,
     ButtonModule,
+    DialogModule,
   ],
   templateUrl: './chat-dialog.component.html',
   styleUrl: './chat-dialog.component.scss',
 })
 export class ChatDialogComponent implements OnInit {
+  displayChatbotDialog: boolean = false;
   messages: Observable<Message[]> = new Observable<Message[]>();
   formValue: string = '';
   isEmpty: boolean = true;
@@ -36,6 +39,14 @@ export class ChatDialogComponent implements OnInit {
     this.messages = this.chat.conversation
       .asObservable()
       .pipe(scan((acc, value) => acc.concat(value)));
+  }
+
+  showDialog() {
+    this.displayChatbotDialog = true;
+  }
+
+  hideDialog() {
+    this.displayChatbotDialog = false;
   }
 
   updateIsEmpty() {
