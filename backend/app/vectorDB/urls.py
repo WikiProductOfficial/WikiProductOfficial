b5d9bfe2ddf446307fd33ee1642e9fc479c90194
FerasAlShateeb <130275283+FerasAlShateeb@users.noreply.github.com>
2024-04-24 00:27:06 +0300

    Add chromadb
    Adding chromadb as a service

--- backend/app/vectorDB/urls.py
+++ backend/app/vectorDB/urls.py
@@ -2,5 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("vectorDB/", views.vector_search, name="vector_search"),
+    path("heartbeat/", views.heartbeat, name="heartbeat"),
+    path("peek/", views.peek, name="peek"),
+    path("collections/", views.list_collections, name="list_collections"),
+    path("similar_by_id/", views.similar_by_id, name="similar_by_id"),
+    path("similar_by_text/", views.similar_by_text, name="similar_by_text"),
 ]
--- backend/app/vectorDB/views.py
+++ backend/app/vectorDB/views.py
@@ -1,6 +1,5 @@
 # Django imports
 from django.shortcuts import render, redirect
-import os
 
 # Django REST Framework imports
 from rest_framework.decorators import api_view
@@ -10,41 +9,149 @@ from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
-import chromadb
-from chromadb.config import Settings
+# Chromadb imports
+from .chromadb import get_items_collection, get_client
 
-CHROMA_SETTINGS = Settings(
-    allow_reset=True,
-    anonymized_telemetry=False,
-)
-
-CHROMA_CLIENT = chromadb.HttpClient(
-    host=os.environ.get('CHROMA_HOST_NAME'),  # Replace with 'localhost' for development
-    port=os.environ.get('CHROMA_HOST_PORT'),
-    settings=CHROMA_SETTINGS
-    )
+# CONSTANTS
+CHROMA_CLIENT= get_client()
+ITEM_COLLECTION = get_items_collection()
 
 @api_view(['GET'])
-def vector_search(request):
-    # Access ChromaDB client
-    client = CHROMA_CLIENT
-
-    # Create or get a collection
-    collection = client.get_or_create_collection(name="my_collection")
-
-    collection.add(
-        documents=["This is a document", "This is another document"],
-        metadatas=[{"source": "my_source"}, {"source": "my_source"}],
-        ids=["id1", "id2"]
-    )
-    
-    # Perform vector operations...
-    results = collection.query(
-        query_texts=["This is a query document"],
-        n_results=1
-    )
-    
+def heartbeat(request):
     return Response({
-        "result": results
+        "result": CHROMA_CLIENT.heartbeat(),
     })
 
+@swagger_auto_schema(
+    method='get',  # Ensure this matches the HTTP method in @api_view
+    manual_parameters=[
+        openapi.Parameter(
+            name='limit',
+            in_=openapi.IN_QUERY,
+            description="""Insert the limit for how many records to peek on. (Default is 10)
+                            It's recommended not to go over 20""",
+            type=openapi.TYPE_INTEGER,
+            required=False,
+        ),
+    ]
+)
+@api_view(['GET'])
+def peek(request):
+    try:
+        limit = int(request.query_params.get('limit', 10))  # Get the limit query parameter
+        return Response({
+            "result": ITEM_COLLECTION.peek(limit= limit),
+        })
+    except:
+        return Response({
+            "message": "limit is not a number",
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+@swagger_auto_schema(
+    method='get',  # Ensure this matches the HTTP method in @api_view
+    manual_parameters=[
+        openapi.Parameter(
+            name='id',
+            in_=openapi.IN_QUERY,
+            description='Insert the item\'s id.',
+            type=openapi.TYPE_INTEGER,
+            required=True,
+        ),
+        openapi.Parameter(
+            name='n',
+            in_=openapi.IN_QUERY,
+            description='Insert the number of similar items. (default is 10)',
+            type=openapi.TYPE_INTEGER,
+            required=False,
+        ),
+    ]
+)
+@api_view(['GET'])
+def similar_by_id(request):
+    try:
+        id = request.query_params.get('id', '')  # Get the id query parameter
+        n = int(request.query_params.get('n', 10))  # Get the n query parameter
+        if id:
+            result= get_similar(id=id, n=n)
+            return Response({
+                "result": result,
+            })
+            
+        else:
+            return Response({"result": "There is no id"}, status= status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({"result": str(e)}, status= status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@swagger_auto_schema(
+    method='get',  # Ensure this matches the HTTP method in @api_view
+    manual_parameters=[
+        openapi.Parameter(
+            name='query',
+            in_=openapi.IN_QUERY,
+            description='Insert the text to search the similarity with.',
+            type=openapi.TYPE_STRING,
+            required=True,
+        ),
+        openapi.Parameter(
+            name='n',
+            in_=openapi.IN_QUERY,
+            description='Insert the number of similar items. (default is 10)',
+            type=openapi.TYPE_INTEGER,
+            required=False,
+        ),
+    ]
+)
+@api_view(["GET"])
+def similar_by_text(request):
+    try:
+        query = request.query_params.get('query', '')  # Get the "query" query parameter
+        n = int(request.query_params.get('n', 10))  # Get the n query parameter
+        if query:
+            result= get_similar(text=query, n=n)
+            return Response({
+                "result": result,
+            })
+            
+        else:
+            return Response({"result": "There is no text"}, status= status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({"result": str(e)}, status= status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(["GET"])
+def list_collections(request):
+    return Response({
+        "result": CHROMA_CLIENT.list_collections(),
+    })
+
+
+# Functions for the API
+def get_item(id):
+    item= ITEM_COLLECTION.get(
+        ids=[id],
+        include=["embeddings"]
+    )
+    return item
+
+def get_similar(id=0, text="", n=10):
+    if id and text:
+        result= Response({
+            "message": "choose one method"
+        }, status= status.HTTP_400_BAD_REQUEST)
+    elif id:
+        result= ITEM_COLLECTION.query(
+            query_embeddings= get_item(id)["embeddings"],
+            n_results= n,
+            include= [], # to return just the item IDs
+        )
+    elif text:
+        result= ITEM_COLLECTION.query(
+            query_texts= text,
+            n_results= n,
+            include=["documents"], # to return just the item IDs
+        )
+    else:
+        result= Response({
+            "message": "choose one method"
+        }, status= status.HTTP_400_BAD_REQUEST)
+    
+    return result
