2e38d9383da88f3c1ac981f092fdbe0234bf45f9
FerasAlShateeb <130275283+FerasAlShateeb@users.noreply.github.com>
2024-04-25 19:05:42 +0300

    Add Auth for chromadb

--- backend/app/vectorDB/models.py
+++ backend/app/vectorDB/models.py
@@ -9,11 +9,17 @@ from chromadb.utils import embedding_functions
 CHROMA_SETTINGS = Settings(
     allow_reset=True,
     anonymized_telemetry=False,
+    chroma_client_auth_credentials= os.environ.get("CHROMA_SERVER_AUTHN_CREDENTIALS"),
+    chroma_client_auth_provider= os.environ.get("CHROMA_CLIENT_AUTHN_PROVIDER"),
 )
 
 CHROMA_CLIENT = chromadb.HttpClient(
     host=os.environ.get('CHROMA_HOST_NAME'),  # Replace with 'localhost' for development
     port=os.environ.get('CHROMA_HOST_PORT'),
+    # ssl= True,
+    headers= {
+        os.environ.get("CHROMA_AUTH_TOKEN_TRANSPORT_HEADER"):os.environ.get("CHROMA_SERVER_AUTHN_CREDENTIALS"),
+    },
     settings=CHROMA_SETTINGS
 )
 
