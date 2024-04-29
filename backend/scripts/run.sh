40f9c358356fc1f204e573ddb4af1cdc0b431f6c
Ammar <ammaralmajed7@gmail.com>
2024-04-29 14:35:57 +0300

    Adding new env variables

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -13,7 +13,7 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsu
-wget "https://www.dropbox.com/scl/fi/ej5650b6vktwhlx8tq31y/clean_warehouse.pkl?rlkey=5xc03h176lcg8nz9j15z84p4a&st=2gqj4xmd&dl=0" -O "../app/scripts/clean_warehouse.pkl"
+wget "$DOWNLOAD_URL" -O "../app/scripts/clean_warehouse.pkl"
 python manage.py runscript load --script-args "$LOAD_START" "$LOAD_END"
 
 uwsgi --socket :8000 --workers 4 --master --enable-threads -b 32768 --module app.wsgi
