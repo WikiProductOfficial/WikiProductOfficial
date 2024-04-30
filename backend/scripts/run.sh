c6b3924e915133d29b070d973ec1c71c3175ba9e
Ammar <ammaralmajed7@gmail.com>
2024-04-30 14:12:00 +0300

    -nc arg

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -13,7 +13,7 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsu
-# wget "$DOWNLOAD_URL" -O "../app/scripts/clean_warehouse.pkl"
+wget -nc "$DOWNLOAD_URL" -O "../app/scripts/clean_warehouse.pkl"
 # python manage.py runscript load --script-args "$LOAD_START" "$LOAD_END"
 
 uwsgi --socket :8000 --workers 4 --master --enable-threads -b 32768 --module app.wsgi
